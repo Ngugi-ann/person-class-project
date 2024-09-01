049515838386a6a2258574f691d8c4c0c77313ae
Ngugi-ann <ngugiann87@gmail.com>
2024-09-01 16:10:37 +0300

    Initial commit with Person class implementation

---
+++ person.py
@@ -0,0 +1,12 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def introduce(self):
+        print(f"Hello, my name is {self.name}. I am {self.age} years old and I am a {self.gender}.")
+
+if __name__ == "__main__":
+    person1 = Person("Dun", 35, "male")
+    person1.introduce()
